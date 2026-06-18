0e9ce05b0184d55da244d554e21fabc772f89aeb
David Corrales <github.ferocity308@passmail.com>
2026-06-18 17:52:07 -0500

    Proyecto por capas

--- src/main/java/com/platzi/play/web/controller/HelloController.java
+++ src/main/java/com/platzi/play/web/controller/HelloController.java
@@ -1,5 +1,6 @@
-package com.platzi.play;
+package com.platzi.play.web.controller;
 
+import com.platzi.play.domain.service.PlatziPlayAiService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,7 @@ public class HelloController {
         this.platziPlayAiService = platziPlayAiService;
     }
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String hello(){
         return this.platziPlayAiService.generateGreeting(plataform);
     }
